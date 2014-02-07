41caf272afb5eb1781dcc91f38c0493a1467f73d
josephnle <josephle@me.com>
2014-02-06 19:53:55 -0800

    Added initial files including Foundation and basic home page.

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Gembyte | Home</title>
+  <link rel="stylesheet" href="css/foundation.css" />
+  <script src="js/vendor/modernizr.js"></script>
+</head>
+<body>
+
+<nav class="top-bar" data-topbar>
+  <ul class="title-area">
+    <li class="name">
+    </li>
+    <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
+  </ul>
+
+  <section class="top-bar-section">
+    <!-- Right Nav Section -->
+    <ul class="right">
+      <li class="active"><a href="#">Join</a></li>
+    </ul>
+
+    <!-- Left Nav Section -->
+    <ul class="left">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">How It Works</a></li>
+    </ul>
+  </section>
+</nav>
+
+<div class="row">
+  <div class="large-12">
+  </div>
+</div>
+
+<div class="row">
+  <div class="small-12 large-6 columns">
+    <h3>BLOG</h3>
+    <p>Stay up to date with development</p>
+    <a href="#" class="button">LEARN MORE</a>
+  </div>
+  <div class="small-12 large-6 columns">
+    <h3>HOW IT WORKS</h3>
+    <p>See a demo of how our product works</p>
+    <a href="#" class="button">VIDEO</a>
+  </div>
+</div>
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/foundation.min.js"></script>
+<script>
+  $(document).foundation();
+</script>
+</body>
+</html>
