aba465727eca24a71b1e24a03df7f0126e262158
ajermaky <ajermaky@ucsd.edu>
2014-02-09 20:14:45 -0800

    Added cutenews files.
    Created blog.php and implements showing latest blog, and then each
    subsequent blog will be shown in two columns under it. To accomplish
    this, the two templates need to be implemented by logging into an
    account and updating the templates there

---
+++ blog.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Gembyte | Home</title>
+  <link rel="stylesheet" href="css/foundation.css" />
+  <script src="js/vendor/modernizr.js"></script>
+</head>
+<body>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<nav class="top-bar" data-topbar>
+  <ul class="title-area">
+    <li class="name">
+    </li>
+    <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
+  </ul>
+
+  <section class="top-bar-section">
+    <!-- Right Nav Section -->
+    <ul class="right">
+      <li class="active"><a href="#">Join</a></li>
+    </ul>
+
+    <!-- Left Nav Section -->
+    <ul class="left">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">How It Works</a></li>
+    </ul>
+  </section>
+</nav>
+
+<div class="row">
+	<div class="large-12 columns">
+		HI
+	</div>
+</div>
+<div class="row">
+	<div class="large-12 columns">
+		<div class="large-8 columns">	
+			<?php
+				
+				$template = "Mainstory";
+				$number = 1;
+				include("./cutenews/show_news.php");
+			?>
+			<div class="row">
+				<div class="large-12 columns" style="float:left;">
+					<?php
+						$number = 0;
+						$start_from = 1;
+						$template = "2Columns";
+						
+						
+						include("./cutenews/show_news.php");
+					?>
+				</div>
+				
+			</div>		
+			<div class="large-4 columns">Twitter</div>
+		</div>
+	</div>
+</div>
+
+<div class="row">
+  <div class="small-12 large-6 columns">
+    <h3>BLOG</h3>
+    <p>Stay up to date with development</p>
+    <a href="#" class="button">LEARN MORE</a>
+	
+  </div>
+  <div class="small-12 large-6 columns">
+    <h3>HOW IT WORKS</h3>
+    <p>See a demo of how our product works</p>
+    <a href="#" class="button">VIDEO</a>
+  </div>
+</div>
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/foundation.min.js"></script>
+<script>
+  $(document).foundation();
+</script>
+</body>
+</html>
