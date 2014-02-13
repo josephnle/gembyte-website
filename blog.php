699ed0863087a2a351a08727c14cc913278de313
ajermaky <ajermaky@ucsd.edu>
2014-02-12 18:54:21 -0800

    removed extra facebook scripts

--- blog.php
+++ blog.php
@@ -9,14 +9,6 @@
 </head>
 <body>
 
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
 
 <nav class="top-bar" data-topbar>
   <ul class="title-area">
@@ -34,7 +26,7 @@
     <!-- Left Nav Section -->
     <ul class="left">
       <li><a href="#">Home</a></li>
-      <li><a href="#">Blog</a></li>
+      <li><a href="./blog.php">Blog</a></li>
       <li><a href="#">How It Works</a></li>
     </ul>
   </section>
@@ -66,12 +58,12 @@
 					?>
 				</div>
 				
-			</div>	
+				
+			</div>
 		</div>
 		<div class="large-4 columns">
 			Twitter
 		</div>
-		
 	</div>
 </div>
 
@@ -94,5 +86,7 @@
 <script>
   $(document).foundation();
 </script>
+
+
 </body>
 </html>
